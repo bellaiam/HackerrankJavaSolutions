f6d28f35b4b5bf54af0eacfe6d8d51d273bfbc89
Bella <bellaiam@uw.edu>
2022-12-10 17:42:06 -0800

    initial

--- src/JavaHashSet.java
+++ src/JavaHashSet.java
@@ -1,22 +1,16 @@
 import java.util.*;
 
-public class JavaHashSet {
-
-    public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        Scanner input = new Scanner(System.in);
-        Set <String> names = new HashSet <String>();
-        int num = input.nextInt();
-        String [] leftName = new String[num];
-        String [] rightName = new String[num];
-        for (int i = 0; i < num; i++) {
-            leftName[i] = input.next();
-            leftName[i] = input.next();
+    public class JavaHashSet {
+        public static void main(String[] args) {
+            /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+            Scanner input = new Scanner(System.in);
+            Set <String> names = new HashSet <>();
+            int num = input.nextInt();
+            input.nextLine();
+            for (int i = 0; i < num; i++) {
+                names.add(input.nextLine());
+                System.out.println(names.size());
+            }
+            input.close();
         }
-        for (int i = 0; i < num; i++) {
-            names.add(leftName[i] + " " + rightName[i]);
-            System.out.println(names.size());
-        }
-        input.close();
     }
-}
